dc5bbbe6963662a4af534f53994c1bf0bf5c4fb3
Praveen <praveen28saxena@gmail.com>
2020-06-15 11:29:37 +0530

    new files for this check

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ var socketIO = require('socket.io');
 var fs = require("fs");
 var fileServer = new(nodeStatic.Server)();
 
-// var app = http.createServer(function(req, res) {
-//   fileServer.serve(req, res);
-// }).listen(8080);
+var privateKey = fs.readFileSync("sslcert/learnomaprivate.key", "utf8");
+var certificate = fs.readFileSync("sslcert/learnomacertificate.crt", "utf8");
 
-var privateKey = fs.readFileSync("sslcert/private.key", "utf8");
-var certificate = fs.readFileSync("sslcert/certificate.crt", "utf8");
 var credentials = { key: privateKey, cert: certificate };
 
 var app = https.createServer(credentials, function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
+
+// var app = http.createServer(function(req, res) {
+//   fileServer.serve(req, res);
+// }).listen(8080);
 
 
 var io = socketIO.listen(app);
@@ -49,15 +50,17 @@ io.sockets.on('connection', function(socket) {
       log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
 
-    } else if (numClients === 1) {
+    } else {
       log('Client ID ' + socket.id + ' joined room ' + room);
       io.sockets.in(room).emit('join', room);
       socket.join(room);
       socket.emit('joined', room, socket.id);
       io.sockets.in(room).emit('ready');
-    } else { // max two clients
-      socket.emit('full', room);
-    }
+    } 
+    // else { 
+    //   // max two clients
+    //   socket.emit('full', room);
+    // }
   });
 
   socket.on('ipaddr', function() {
